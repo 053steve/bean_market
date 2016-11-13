bf958682f3a890ad5208703b0adb1d062e9fa607
Steve Suranant <053steve@gmail.com>
2016-11-13 13:38:43 +0800

    Commit before merge, myreceipt finished

--- api/controllers/FrontController.js
+++ api/controllers/FrontController.js
@@ -29,24 +29,77 @@ var FrontController = {
     },
 
     MyReceipt: function(req, res) {
-        Upload.find().exec(function (err, uploads){
+        Upload.find().exec(function (err, receipts){
           if (err) {
             return res.serverError(err);
           }
-          console.log(uploads);
-          return res.view({
-                page: 'myreceipt',
-                view: 'front/myreceipt'
-            });
+          var results = [];
+          // assuming openFiles is an array of file names
+            async.each(receipts, function(receipt, cb) {
+                var newpath = receipt.path.split("../../assets/");
+                console.log('newpath ' + newpath[1]);
+                receipt.newpath = newpath[1];
+                results.push(receipt)
+                // Perform operation on file here.
+                console.log('Processing file ' + JSON.stringify(receipt));
+
+                cb()
+            }, function(err) {
+                // if any of the file processing produced an error, err would equal that error
+                if( err ) {
+                  // One of the iterations produced an error.
+                  // All processing will now stop.
+                  console.log('A file failed to process');
+                } else {
+                  console.log('All files have been processed successfully');
+                }
+
+                return res.view({
+                    page: 'myreceipt',
+                    view: 'front/myreceipt',
+                    receipts: results
+                });
+            });          
+          
         });
         
     },
 
     UserReceipt: function(req, res) {
-        return res.view({
-            page: 'userreceipt',
-            view: 'front/myreceipt'
+        Upload.find().exec(function (err, receipts){
+          if (err) {
+            return res.serverError(err);
+          }
+          var results = [];
+          // assuming openFiles is an array of file names
+            async.each(receipts, function(receipt, cb) {
+                var newpath = receipt.path.split("../../assets/");
+                console.log('newpath ' + newpath[1]);
+                receipt.newpath = newpath[1];
+                results.push(receipt)
+                // Perform operation on file here.
+                console.log('Processing file ' + JSON.stringify(receipt));
+
+                cb()
+            }, function(err) {
+                // if any of the file processing produced an error, err would equal that error
+                if( err ) {
+                  // One of the iterations produced an error.
+                  // All processing will now stop.
+                  console.log('A file failed to process');
+                } else {
+                  console.log('All files have been processed successfully');
+                }
+
+                return res.view({
+                    page: 'userreceipt',
+                    view: 'front/userreceipt',
+                    receipts: results
+                });
+            });          
+          
         });
+        
     },
 
 }
